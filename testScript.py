bd061a837f524b3966f058e0cef764eed354b291
Gabriel Quiroz <gquiroz@hmc.edu>
2017-07-31 16:03:52 -0700

    Changed graph

--- testScript.py
+++ testScript.py
@@ -9,11 +9,11 @@ from graphAndGrid import *
 import networkx as nx
 
 G = nx.Graph()
-nodes = [0, 1, 2, 3, 4, 5, 6]
-edges = [(0, 1), (0, 2), (0, 5), (1, 2), (1, 4), (1, 6), (2, 5), (2, 3), (3, 5), (3, 4), (4, 6)]
+nodes = [0, 1, 2, 3, 4]
+edges = [(0, 1), (0, 2), (1, 2), (1, 4), (2, 3), (3, 4)]
 G.add_nodes_from(nodes)
 G.add_edges_from(edges)
-cartCoords = [(1, 1), (1, 3), (1.5, 1.5), (2, 2), (1, 1.5), (1.75, 1.25), (5, 5)]
+cartCoords = [(1, 1), (1, 3), (1.5, 1.5), (2, 2), (1, 1.5)]
 dic = {}
 for i in range(len(cartCoords)):
 	dic.update({nodes[i]: cartCoords[i]})
