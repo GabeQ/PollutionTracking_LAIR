913fc0c38ce4dfe7d25845f395a1611683e8dd7a
Gabriel Quiroz <gquiroz@hmc.edu>
2017-08-07 16:46:05 -0700

    Added offset
    Changed to COM4

--- nmea_reader.py
+++ nmea_reader.py
@@ -3,7 +3,7 @@ import serial
 
 ser = serial.Serial()
 ser.baudrate = 4800
-ser.port = 'COM5'
+ser.port = 'COM4'
 ser.open()
 
 file = open('coordinates.txt', 'w+')
@@ -13,18 +13,28 @@ file = open('coordinates.txt', 'w+')
 #nodes = graph.nodes()
 #set_cart_coords(graph, originCoord=s)
 
+latOffset = .042457
+longOffset = -.284534
+
 bad_line = ser.readline()
 coord_list = []
-while len(coord_list) < 50:
+while len(coord_list) < 200:
 	line = str(ser.readline())
 	dat = list(line.split(','))
 
 	if dat[0] == "b'$GPGGA":
-		latitude = float(dat[2]) /100
-		longitude = float(dat[4]) / -100
+		latitude = float(dat[2]) / 100
+		longitude = float(dat[4]) / 100
+		if dat[3] == 'S':
+			latitude *= -1
+		if dat[5] == 'W':
+			longitude *= -1
+		latitude += latOffset
+		longitude += longOffset
 		coordinate = (latitude, longitude)
 		coord_list.append(coordinate)
-		file.write(str(coordinate) + '\n')
+		numSat = dat[7]
+		file.write(str(coordinate)  +  '\n')
 		print(coordinate)
 
 	if dat[0] == "b'$GPGSA":
@@ -32,10 +42,16 @@ while len(coord_list) < 50:
 
 	if dat[0] == "b'$GPRMC":
 		latitude = float(dat[3]) /100
-		longitude = float(dat[5]) / -100
+		longitude = float(dat[5]) / 100
+		if dat[4] == 'S':
+			latitude *= -1
+		if dat[6] == 'W':
+			longitude *= -1
+		latitude += latOffset
+		longitude += longOffset
 		coordinate = (latitude, longitude)
 		coord_list.append(coordinate)
-		file.write(str(coordinate) + '\n')
+		file.write(str(coordinate) +  '\n')
 
 	if dat[0] == "b'$GPGSV":
 		continue
